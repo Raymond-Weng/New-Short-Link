d73fc7087c30c7cb5233245289bdb7668dd2d5e9
Raymond <yuhongweng0501@gmail.com>
2025-03-28 17:19:57 +0800

    Create Repo

---
+++ src/main/java/com/raymondweng/newshortlink/NewShortLinkApplication.java
@@ -0,0 +1,13 @@
+package com.raymondweng.newshortlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewShortLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewShortLinkApplication.class, args);
+	}
+
+}
