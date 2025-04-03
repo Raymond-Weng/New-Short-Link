b8ce799dd9ed144b51766df7a7fd73b3ce96ae97
Raymond <yuhongweng0501@gmail.com>
2025-04-03 15:51:13 +0800

    try make a index page

---
+++ src/main/java/com/raymondweng/newshortlink/SiteController.java
@@ -0,0 +1,12 @@
+package com.raymondweng.newshortlink;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SiteController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
